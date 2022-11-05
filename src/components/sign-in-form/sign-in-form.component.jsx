8c27de4033443c22a8cb4c1f40d3a9a3ea07cfc1
YojimboSlice <jamesryanlan@gmail.com>
2022-11-05 15:07:26 -0700

    Add onAuthStateChanged() to user.context
    Keeping track of whether a user is signed in and signed out in too many places such as sign-in and sign-up component. Using onAuthStateChanged() allows for an open observer that is listening to whether a user is signed in or signed out.

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,4 +1,3 @@
-import { getAuth } from 'firebase/auth';
 import { useState } from 'react';
 
 import FormInput from '../form-input/form-input.component';
@@ -29,19 +28,17 @@ const SignInForm = () => {
   };
 
   const signInWithGoogle = async () => {
-    const { user } = await signInWithGooglePopup();
-    await createUserDocumentFromAuth(user);
+    await signInWithGooglePopup();
   };
 
   const handleSubmit = async event => {
     event.preventDefault();
 
     try {
-      const response = await signInAuthUserWithEmailAndPassword(
+      const { user } = await signInAuthUserWithEmailAndPassword(
         email,
         password,
       );
-      console.log(response);
       resetFormFields();
     } catch (error) {
       switch (error.code) {
@@ -64,7 +61,7 @@ const SignInForm = () => {
   return (
     <div className='sign-up-container'>
       <h2>Already have an account?</h2>
-      <span>Sign up with your email and password</span>
+      <span>Sign in with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label='Email'
--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,14 +1,12 @@
-import { getAuth } from 'firebase/auth';
 import { useState } from 'react';
 
 import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
 
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
-
-import Button from '../button/button.component';
 
 import './sign-up-form.styles.scss';
 
@@ -22,8 +20,6 @@ const defaultFormFields = {
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
-
-  console.log(formFields);
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
